aaf5c26cdce8bf21c8e11c6085477416fe26a98c
Christine Zheng <christinezheng@Meltons-MacBook-Pro.local>
2022-02-19 09:14:25 -0800

    messages with additional confirm abouts

--- app.js
+++ app.js
@@ -44,9 +44,23 @@ const promptUser = () => {
 
     },
     {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+
+    {
         type: 'input',
         name: 'about',
-        message: 'provide additional info about yourself:'
+        message: 'provide additional info about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+              return true;
+            } else {
+              return false;
+            }
+          }
     }
   ]);
 };
